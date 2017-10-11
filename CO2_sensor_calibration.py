1f5be6b51360a5605a7a7e7ec4b9f61d5aa1baa6
m-jahn <michael.jahn@scilifelab.se>
2017-10-11 11:47:31 +0200

    new CO2 sensor calibration against nitrogen (N2)

---
+++ CO2_sensor_calibration.py
@@ -0,0 +1,64 @@
+# author: Michael Jahn
+# date: 2017-10-11
+# coding=utf-8
+"""
+Connect to CO2 sensor and take one measurement
+"""
+# loading libraries
+import sys
+import serial
+import re
+import os
+import time
+import numpy
+
+
+# function to define and connect serial device
+def connectSens(serialID):
+    return serial.Serial(port=serialID, timeout=1)
+
+
+# take one measurement and format ouput
+def calibrateToN2(sensor):
+    # ask sensor to calibrate to N2
+    sensor.write('U\r\n')
+    print('Calibration done, message: ' + sensor.readline())
+    time.sleep(1)
+    # request series of 5 measurements and print mean CO2 conc
+    print('Performing test measurement... ')
+    reads = list()
+    for i in range(1,6):
+        sensor.write('Q\r\n')
+        # read measurement from sensor cache and print
+        read = sensor.readline()
+        # re-format reads
+        read = re.sub('\r\n|.Z.|.z', '', read)[0:5]
+        print(read); reads.append(read)
+        time.sleep(1)
+    reads = [int(x) for x in reads]
+    return numpy.mean(reads)
+
+
+# LOOP OVER ALL CO2 SENSORS AND CALIBRATE
+# function to find attached CO2 sensors in /dev
+co2sensors = filter(lambda x: 'ttyCO2' in x, os.listdir('/dev'))
+# throw error when no sensor is connected
+if len(co2sensors) == 0:
+    raise ValueError('No sensors named "ttyCO2" connected')
+
+# calibration loop
+for co2sensor in co2sensors:
+    sensor = connectSens('/dev/' + co2sensor)
+    # check if connection is open
+    print('Connecting to sensor ' + co2sensor)
+    if not sensor.is_open: sensor.open()
+    else: print("Connection already open")
+    #
+    # zero calibrate to molecular nitrogen N2
+    meanConc = calibrateToN2(sensor) 
+    print('Mean CO2 after blanking: ' + str(meanConc*10) + ' ppm')
+    #
+    # close connection
+    sensor.close()
+
+exit()
