de258cf684a6b99a5a0de82af201f2c4e2196333
m-jahn <michael.jahn@scilifelab.se>
2017-10-11 11:45:25 +0200

    CO2 measurement now for all connected sensors

--- CO2_measurement.py
+++ CO2_measurement.py
@@ -1,5 +1,5 @@
 # author: Michael Jahn
-# date: 2017-04-10
+# date: 2017-10-10
 # coding=utf-8
 """
 Connect to CO2 sensor and take one measurement
@@ -8,21 +8,17 @@ Connect to CO2 sensor and take one measurement
 import sys
 import serial
 import re
+import os
 import datetime as time
 
+
+# function to define and connect serial device
 def connectSens(serialID):
-    # define and connect serial device, name must be known
     return serial.Serial(port=serialID, timeout=1)
 
-co2sensor = connectSens('/dev/ttyCO2_1')
-# check if connection is open
-if not co2sensor.is_open: co2sensor.open()
-else:
-    print("Connection already open")
 
-
+# take one measurement and format ouput
 def makeMeasure(sensor):
-	
     # set mode of sensor according to COZIR manual
     # best mode is 2, constant measurement and response on request
     sensor.write('K 2\r\n')
@@ -33,19 +29,35 @@ def makeMeasure(sensor):
     read = sensor.readline()
     # first number is raw value, 2nd is digitally filtered
     # add time and date to CO2 reading
-    read = read + ' ' + time.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n'
+    read = read + ' ' + co2sensor + ' ' + time.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n'
     # format string as a tab separated text
     read = re.sub('\r\n|.Z.|.z', '', read)
     return read
 
-# append measurement to standard file
-if len(sys.argv) == 2 and isinstance(sys.argv[1], basestring):
-    fileName = "/home/multicultivator/multicultivator/data/" + str(sys.argv[1])
-else:
-    fileName = "/home/multicultivator/multicultivator/data/" + time.datetime.now().strftime('%Y%m%d') + "_CO2.txt"
-with open(fileName, "a") as co2data:
-    co2data.write(makeMeasure(co2sensor))
 
-# close connection
-co2sensor.close()
+# LOOP OVER ALL CO2 SENSORS AND MAKE MEASUREMENT
+# function to find attached CO2 sensors in /dev
+co2sensors = filter(lambda x: 'ttyCO2' in x, os.listdir('/dev'))
+# throw error when no sensor is connected
+if len(co2sensors) == 0:
+    raise ValueError('No sensors named "ttyCO2" connected')
+
+# measurement loop
+for co2sensor in co2sensors:
+    sensor = connectSens('/dev/' + co2sensor)
+    # check if connection is open
+    if not sensor.is_open: sensor.open()
+    else: print("Connection already open")
+    #
+    # append measurement to standard file
+    if len(sys.argv) == 2 and isinstance(sys.argv[1], basestring):
+        fileName = "/home/multicultivator/multicultivator/data/" + str(sys.argv[1])
+    else:
+        fileName = "/home/multicultivator/multicultivator/data/" + time.datetime.now().strftime('%Y%m%d') + "_CO2.txt"
+    with open(fileName, "a") as co2data:
+        co2data.write(makeMeasure(sensor))
+    #
+    # close connection
+    sensor.close()
+
 exit()
