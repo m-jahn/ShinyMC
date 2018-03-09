a531e58d93620d183b50ecde82dc24976b2d5469
m-jahn <michael.jahn@scilifelab.se>
2018-03-09 09:52:06 +0100

    show date time stamp on OD plots

--- app.R
+++ app.R
@@ -236,6 +236,10 @@ server <- shinyServer(function(input, output) {
         panel.xyplot(unique(x), y[seq(1, length(y), 2)]/y[seq(2, length(y), 2)], 
           col=grey(0.7), cex=0.2)
         }
+        if (panel.number()==1) {
+          panel.text(input$UserXlim[2]*0.05, input$UserYlim[2]*0.85, cex=0.5, col=grey(0.4),
+            pos=4, labels=paste0("last measurement: \n", data[nrow(data), "time"]))
+        }
         panel.superpose(x, y, ...)
         },
       panel.groups=function(x, y, ...){
